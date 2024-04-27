cd48af9218b8addc1cb47ea4b51ca55543430a4b
sam-aydlette <sam.aydlette@gmail.com>
2024-04-26 21:59:24 -0400

    cleaning up component outputs for readability

--- commands/components.py
+++ commands/components.py
@@ -1,6 +1,22 @@
 def list_components(oscal_ssp):
     components = oscal_ssp["system-security-plan"]["system-implementation"]["inventory-items"]
     for component in components:
+        print(f"Description: {component['description']}")
 
-        print(f"Component: {component["implemented-components"]}")
-        print("\n")
+        print("Properties:")
+        for prop in component["props"]:
+            name = prop["name"]
+            value = prop["value"]
+            prop_class = prop.get("class", "")
+            print(f"  {name}: {value} ({prop_class})")
+
+        print("Implemented Components:")
+        implemented_components = component.get("implemented-components", [])
+        for imp_comp in implemented_components:
+            component_props = imp_comp.get("props", [])
+            for comp_prop in component_props:
+                name = comp_prop["name"]
+                value = comp_prop["value"]
+                print(f"    {name}: {value}")
+
+        print()
