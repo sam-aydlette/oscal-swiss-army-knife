3ad58ca4d5d2bb8f20b2c982201131de94a92b5b
sam-aydlette <sam.aydlette@gmail.com>
2024-04-26 21:43:53 -0400

    updating component command

--- commands/components.py
+++ commands/components.py
@@ -1,5 +1,6 @@
 def list_components(oscal_ssp):
     components = oscal_ssp["system-security-plan"]["system-implementation"]["inventory-items"]
     for component in components:
-        print(f"Component: {component["props"]}")
+
+        print(f"Component: {component["implemented-components"]}")
         print("\n")
